ddca5fe48e0237f15447a91562e9726fb6c70e8d
krupa2029 <kjkotia10@gmail.com>
2021-10-10 18:41:43 +0530

    Modf: Cart-Button NewCompo: CartIcon, HeaderCartButton

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,17 +1,18 @@
 import React, { Fragment } from "react";
 
-import mealsImage from '../../assets/meals.jpg';
-import classes from './Header.module.css';
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-          <h1>Eateria</h1>
-          <button>Cart</button>
+        <h1>Eateria</h1>
+        <HeaderCartButton />
       </header>
-      <div className={classes['main-image']}>
-          <img src={mealsImage} alt='A table full of delicious food!'/>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicious food!" />
       </div>
     </Fragment>
   );
