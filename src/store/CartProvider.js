c63c9a765cc1807a32ecc613c738f1368bd3d6fc
krupa2029 <kjkotia10@gmail.com>
2021-10-12 20:08:00 +0530

    Feat: Remove and Add Items from Cart(Logic) Used: Reducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,8 +8,9 @@ const defaultCartState = {
 };
 
 const cartReducer = (state, action) => {
+
   if (action.type === "ADD") {
-    // const updatedItems = state.items.concat(action.item);
+    
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
 
@@ -37,7 +38,27 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
-  //   else if(action.type === "ADD")
+
+  if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount-1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
   return defaultCartState;
 };
 
