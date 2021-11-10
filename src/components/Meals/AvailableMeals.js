6ee02bad5783d5ee7b6c50467aa75ea90b0d80c1
krupa2029 <kjkotia10@gmail.com>
2021-11-10 20:47:01 +0530

    Connected with Firebase and Fetching data

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,51 +1,50 @@
+import { useEffect, useState } from "react";
+
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 import Card from "../UI/Card";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Double Cheese Margherita Pizza",
-    description:
-      "The ever-popular Margherita - loaded with extra cheese... oodies of it!",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Mysore Masala Dosa",
-    description: "All time favourite mysore masala dosa!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = () => {
-  
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://eateria-c43d9-default-rtdb.firebaseio.com/meals.json"
+      );
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+
+      setMeals(loadedMeals);
+    };
+
+    fetchMeals();
+  }, []);
+
   return (
     <section className={classes.meals}>
       <Card>
-      <ul>
-        {DUMMY_MEALS.map((meal) => (
-          <MealItem
-            id={meal.id}
-            key={meal.id}
-            name={meal.name}
-            description={meal.description}
-            price={meal.price}
-          />
-        ))}
-      </ul>
+        <ul>
+          {meals.map((meal) => (
+            <MealItem
+              id={meal.id}
+              key={meal.id}
+              name={meal.name}
+              description={meal.description}
+              price={meal.price}
+            />
+          ))}
+        </ul>
       </Card>
     </section>
   );
